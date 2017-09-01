6c98f4bf52b1423c98594e0e8a382f6274d4d89f
Selbahc <selbahc@users.noreply.github.comm>
2017-09-01 20:33:36 +0200

    implemented a single player mode

--- pendu.js
+++ pendu.js
@@ -1,3 +1,8 @@
+//Desactiver l'action du formulaire
+document.querySelector('.form-container').addEventListener('submit', (e) => {
+  e.preventDefault();
+})
+
 //Vérifier si lettre saisie figure dans le mot secret, et si oui, la rajouter au mot deviné
 function checkAndReplace(answerArray, letter, secretArray) {
   if (secretArray.indexOf(letter) === -1) remainingAttempts--;
@@ -5,6 +10,12 @@ function checkAndReplace(answerArray, letter, secretArray) {
     if (secretArray[i] == letter) answerArray[i] = letter;
   }
   return answerArray;
+}
+
+//Si le mot à deviner est vide, en piocher un dans la liste
+const pickAWord = (string) => {
+  const random = Math.floor((Math.random() * wordsArray.length) + 1); //wordsArray défini dans mots.js
+  secretWord = wordsArray[random].toUpperCase().split('');
 }
 
 //Nb vies restantes
@@ -18,25 +29,29 @@ var imgElt = document.querySelector('#img-pendu');
 imgElt.innerHTML = '<img src="images/pendu' + remainingAttempts + '.png" alt="avancee pendu">';
 
 //Faire choisir un mot à deviner
-var wordElt = prompt('Choisissez le mot à faire deviner').toUpperCase().split('');
+var secretWord = prompt('Choisissez le mot à faire deviner\n(Laissez vide si vous souhaitez que l\'ordinateur le choisisse pour vous)').toUpperCase().split('');
+secretWord.length < 1 ? pickAWord(secretWord) : secretWord;
+console.log(secretWord.join(''));
 
-//Le mot à afficher et compléter
+//Afficher le nombre de caracteres correspondant au mot secret
 var answerElt = document.querySelector('#answer');
-answerElt.textContent = '_'.repeat(wordElt.length);
+answerElt.textContent = '_'.repeat(secretWord.length);
 var answerContent = answerElt.textContent.split('');
 
-//Lettre essayée
-var attemptElt = document.querySelector('#value-attempt');
+var attemptElt = document.querySelector('#value-attempt'); //Lettre essayée
 
 //Au clic sur le bouton, appeler la fonction checkAndReplace, actualiser le nombre de vies, vider l'input. Le cas échéant, afficher la phrase de victoire ou de game over
 var buttonElt = document.querySelector('#attempt-submit');
 buttonElt.addEventListener('click', function() {
-  answerElt.innerHTML = checkAndReplace(answerContent, attemptElt.value.toUpperCase(), wordElt).join('');
+  answerElt.innerHTML = checkAndReplace(answerContent, attemptElt.value.toUpperCase(), secretWord).join('');
 
   imgElt.innerHTML = '<img src="images/pendu' + remainingAttempts + '.png" alt="avancee pendu">';
   livesElt.innerHTML = 'Encore ' + remainingAttempts + ' essai(s) restant(s)';
   attemptElt.value = '';
 
-  if (answerElt.innerHTML.indexOf('_') == -1) alert('Bravo, Vous avez gagné !');
-  if (remainingAttempts == 0) alert("Vous avez fini pendu !")
+  if (answerElt.innerHTML.indexOf('_') == -1) alert(`Bravo, Vous avez gagné ! \n Il s'agissait bien de ${secretWord.join('')}`);
+  if (remainingAttempts == 0) {
+    alert('Vous avez fini pendu !');
+    answerElt.textContent = secretWord.join('');
+  }
 });
